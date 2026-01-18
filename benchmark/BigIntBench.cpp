e142ba6f4c6e977bb21f013cb0b7283a6c56a339
Touch Sungkawichai <my999n@gmail.com>
2026-01-18 21:28:48 +0000

    new bench again, TODO: improve div, mod, bigmult

--- benchmark/BigIntBench.cpp
+++ benchmark/BigIntBench.cpp
@@ -1,15 +1,20 @@
 #include <benchmark/benchmark.h>
 #include <utils/BigInt.h>
+#include <utils/biggerint.h>
 
 template <typename T> static T make_big(int n)
 {
+  static std::unordered_map<int, T> cache;
+  auto it = cache.find(n);
+  if (it != cache.end()) return it->second;
+
   T x(1);
   for (int i = 1; i <= n; ++i)
   {
     x *= i + 1;
     x += i;
   }
-  return x;
+  return cache[n] = x;
 }
 
 /* ================= ADD ================= */
@@ -100,15 +105,15 @@ static void BM_div_dense(benchmark::State& s) { BM_div_impl<DenseBigInt>(s); }
 static void BM_mod_dec(benchmark::State& s) { BM_mod_impl<DecBigInt>(s); }
 static void BM_mod_dense(benchmark::State& s) { BM_mod_impl<DenseBigInt>(s); }
 
-BENCHMARK(BM_add_dec)->Name("DecBigInt/Add")->Args({128})->Args({512})->Args({2048});
-BENCHMARK(BM_add_dense)->Name("DenseBigInt/Add")->Args({128})->Args({512})->Args({2048});
-BENCHMARK(BM_sub_dec)->Name("DecBigInt/Sub")->Args({128})->Args({512})->Args({2048});
-BENCHMARK(BM_sub_dense)->Name("DenseBigInt/Sub")->Args({128})->Args({512})->Args({2048});
-BENCHMARK(BM_mul_dec)->Name("DecBigInt/Mul")->Args({128})->Args({512})->Args({2048});
-BENCHMARK(BM_mul_dense)->Name("DenseBigInt/Mul")->Args({128})->Args({512})->Args({2048});
-BENCHMARK(BM_div_dec)->Name("DecBigInt/Div")->Args({128})->Args({512})->Args({2048});
-BENCHMARK(BM_div_dense)->Name("DenseBigInt/Div")->Args({128})->Args({512})->Args({2048});
-BENCHMARK(BM_mod_dec)->Name("DecBigInt/Mod")->Args({128})->Args({512})->Args({2048});
-BENCHMARK(BM_mod_dense)->Name("DenseBigInt/Mod")->Args({128})->Args({512})->Args({2048});
+BENCHMARK(BM_add_dec)->Name("DecBigInt/Add")->Args({128})->Args({512})->Args({8192});
+BENCHMARK(BM_add_dense)->Name("DenseBigInt/Add")->Args({128})->Args({512})->Args({8192});
+BENCHMARK(BM_sub_dec)->Name("DecBigInt/Sub")->Args({128})->Args({512})->Args({8192});
+BENCHMARK(BM_sub_dense)->Name("DenseBigInt/Sub")->Args({128})->Args({512})->Args({8192});
+BENCHMARK(BM_mul_dec)->Name("DecBigInt/Mul")->Args({128})->Args({512})->Args({8192})->Args({65536});
+BENCHMARK(BM_mul_dense)->Name("DenseBigInt/Mul")->Args({128})->Args({512})->Args({8192})->Args({65536});
+BENCHMARK(BM_div_dec)->Name("DecBigInt/Div")->Args({128})->Args({512})->Args({8192});
+BENCHMARK(BM_div_dense)->Name("DenseBigInt/Div")->Args({128})->Args({512})->Args({8192});
+BENCHMARK(BM_mod_dec)->Name("DecBigInt/Mod")->Args({128})->Args({512})->Args({8192});
+BENCHMARK(BM_mod_dense)->Name("DenseBigInt/Mod")->Args({128})->Args({512})->Args({8192});
 
 BENCHMARK_MAIN();
